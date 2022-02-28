913b531e79ee2a30ed05890370513fbe52521457
Lusvin Sicaja <lusvin.alex@gmail.com>
2022-02-27 19:40:26 -0600

    primer commit, subida de la tarea 2

---
+++ src/login/Main.java
@@ -0,0 +1,65 @@
+
+package login;
+
+import com.sun.xml.internal.bind.v2.runtime.RuntimeUtil;
+
+public class Main {
+    
+    private String nombre;
+    private String apellido;
+    private String carnet;
+
+    public Main(String nombre, String apellido, String carnet) {
+        this.nombre = nombre;
+        this.apellido = apellido;
+        this.carnet = carnet;
+    }
+
+       
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getApellido() {
+        return apellido;
+    }
+
+    public void setApellido(String apellido) {
+        this.apellido = apellido;
+    }
+
+    public String getCarnet() {
+        return carnet;
+    }
+
+    public void setCarnet(String carnet) {
+        this.carnet = carnet;
+    }
+    
+
+    
+
+    
+    
+    
+    public static void main(String[] args) {
+        
+        
+        
+        Login lg = new Login();
+        lg.setVisible(true);
+        
+
+    }
+
+    @Override
+    public String toString() {
+        return  "Nombre: " + nombre + ", Apellido: " + apellido + ", Carnet: " + carnet;
+    }
+    
+}
